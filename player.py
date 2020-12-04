19b5c54b5cbf400e6d2fad4a8c26d15d1a9c5e48
Kylie <kying@mit.edu>
2020-12-03 23:18:02 -0500

    Update player.py

--- player.py
+++ player.py
@@ -1,3 +1,14 @@
+"""
+Tic-Tac-Toe players using inheritance implementation by Kylie YIng
+YouTube Kylie Ying: https://www.youtube.com/ycubed 
+Twitch KylieYing: https://www.twitch.tv/kylieying 
+Twitter @kylieyying: https://twitter.com/kylieyying 
+Instagram @kylieyying: https://www.instagram.com/kylieyying/ 
+Website: https://www.kylieying.com
+Github: https://www.github.com/kying18 
+Programmer Beast Mode Spotify playlist: https://open.spotify.com/playlist/4Akns5EUb3gzmlXIdsJkPs?si=qGc4ubKRRYmPHAJAIrCxVQ 
+"""
+
 import math
 import random
 
