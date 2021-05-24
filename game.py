f4c768a0d778d48bdd41d2a8b7064bf9d838aa22
Scott Milliorn <scottmilliorn@gmail.com>
2021-05-24 14:31:47 -0700

    set_square, print_tie, reduced complexity

--- game.py
+++ game.py
@@ -76,37 +76,42 @@ class TicTacToe():
 
 
 def play(game, x_player, o_player, print_game=True):
-
     if print_game:
         game.print_board_nums()
 
     letter = 'X'
     while game.empty_squares():
-        if letter == 'O':
-            square = o_player.get_move(game)
-        else:
-            square = x_player.get_move(game)
+        square = set_square(game, x_player, o_player, letter)
         if game.make_move(square, letter):
-
             print_move(game, print_game, letter, square)
-
             if game.current_winner:
                 if print_game:
                     print(letter + ' wins!')
                 return letter  # ends the loop and exits the game
             letter = 'O' if letter == 'X' else 'X'  # switches player
-
         time.sleep(.8)
 
+    print_tie(print_game)
+
+
+def set_square(game, x_player, o_player, letter):
+    if letter == 'O':
+        square = o_player.get_move(game)
+    else:
+        square = x_player.get_move(game)
+    return square
+
+
+def print_tie(print_game):
     if print_game:
         print('It\'s a tie!')
+
 
 def print_move(game, print_game, letter, square):
     if print_game:
         print(letter + ' makes a move to square {}'.format(square))
         game.print_board()
         print('')
-
 
 
 if __name__ == '__main__':
