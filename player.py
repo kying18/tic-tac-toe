2b689aba16d739242dc323ad454b07199cdc7f88
Valik Solorzano <vsolorzanobarboza@zebra.com>
2023-04-21 16:29:15 -0400

    chore: human input is only valid between 0 and 8

--- player.py
+++ player.py
@@ -29,7 +29,7 @@ class HumanPlayer(Player):
         valid_square = False
         val = None
         while not valid_square:
-            square = input(self.letter + '\'s turn. Input move (0-9): ')
+            square = input(self.letter + '\'s turn. Input move (0-8): ')
             try:
                 val = int(square)
                 if val not in game.available_moves():
